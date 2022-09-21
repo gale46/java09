dda3fa5d0182001cedd1a1b153da66311a2e65fb
gale46 <113490808+gale46@users.noreply.github.com>
2022-09-21 11:17:49 +0800

    Add files via upload

---
+++ class07.java
@@ -0,0 +1,22 @@
+package java09;
+
+class a {
+	void power(int x, int n) {
+		Math.pow(x, n);
+		
+	}
+		
+
+}
+
+public class class07 {
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			a a1 = new a();
+			a1.power(2, 5);
+			a1.power(3, 2);
+
+		}
+
+}
+
