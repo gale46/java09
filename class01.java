715da4109e1f6b92033c13218e9ad94a911deef1
gale46 <113490808+gale46@users.noreply.github.com>
2022-09-21 11:21:48 +0800

    Update class01.java

--- class01.java
+++ class01.java
@@ -7,16 +7,16 @@ class CRectangle {
 	private int h;
 	
 	void CRectangle() {
-		width = 10;
+		width = 10;//設數值
 		height = 8;
 		
 		
 	}
 
-	void CRectangle(int w, int h) {
+	void CRectangle(int w, int h) {//傳入w, h
 		width = w;
 		height = h;
-		//width = this.w;
+		//width = this.w;//使用this
 		//height = this.h;
 		
 	}
@@ -29,7 +29,7 @@ public class class01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CRectangle cr = new CRectangle();
+		CRectangle cr = new CRectangle();//建立物件
 		cr.CRectangle();
 		cr.CRectangle(0, 0);
 		
